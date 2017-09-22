0248d89aa8c7f26191f8aff89cbc560903550ec4
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-09-22 15:53:54 +0100

    Fix e2e test

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/e2e/app.e2e-spec.ts
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('client-app App', () => {
+describe('App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('client-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getMainHeading()).toEqual('Hello, world!');
   });
 });
