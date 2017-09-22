95f1b6b4ccb0fa4e85289b99570894da0dd4e9db
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-09-22 15:37:14 +0100

    Add CounterComponent

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/app/app.module.ts
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/app/app.module.ts
@@ -5,22 +5,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    CounterComponent
   ],
   imports: [
       BrowserModule,
       RouterModule.forRoot([
         { path: '', redirectTo: 'home', pathMatch: 'full' },
         { path: 'home', component: HomeComponent },
-        /*
         { path: 'counter', component: CounterComponent },
-        { path: 'fetch-data', component: FetchDataComponent },
-        */
+        //{ path: 'fetch-data', component: FetchDataComponent },
       ])
   ],
   providers: [],
