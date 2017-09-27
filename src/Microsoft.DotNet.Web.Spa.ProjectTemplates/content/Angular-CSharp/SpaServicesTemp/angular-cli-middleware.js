9737b88ef1f02e2aa21ef9214621edf9b686769b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-09-27 12:22:28 +0100

    Make app startup wait until CLI server has started (for client-side bundles)

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/SpaServicesTemp/angular-cli-middleware.js
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/SpaServicesTemp/angular-cli-middleware.js
@@ -1,37 +1,68 @@
 ﻿var childProcess = require('child_process');
+var net = require('net');
+var readline = require('readline');
+var url = require('url');
 
 module.exports = {
     startAngularCliServer: function startAngularCliServer(callback, options) {
-        var deployUrl = '/dist/'; // Should come from .angular-cli.json, but https://github.com/angular/angular-cli/issues/7347
-
-        executeAngularCli([
-            'serve',
-            '--deploy-url', deployUrl,
-            '--extract-css'
-        ]);
-
+        // Start an @angular/cli instance to watch and write SSR
+        // builds to the /dist-server dir
         // TODO: Make server-side rendering optional via flag in Startup.cs
-        // TODO: Make sure we kill the child process when this parent process is killed
-        childProcess.fork(require.resolve('@angular/cli/bin/ng'), [
+        // TODO: Make asp-prerender-module wait until the files exist on disk
+        executeAngularCli([
             'build',
             '-app', 'ssr',
             '--watch',
             '--output-path', 'dist-server'
         ]);
 
-        // TODO: Don't issue callback until both ng processes have started, and
-        // the SSR one has written its output file to disk
-        callback(null, {
-            Port: 4200, // TODO: Determine dynamically (and preferably avoid clashes)
-            PublicPaths: [deployUrl] // TODO: Determine dynamically
+        getOSAssignedPortNumber().then(function (portNumber) {
+            // Start @angular/cli dev server on private port, and pipe its output
+            // back to the ASP.NET host process.
+            // TODO: Support streaming arbitrary chunks to host process's stdout
+            // rather than just full lines, so we can see progress being logged
+            var devServerProc = executeAngularCli([
+                'serve',
+                '--port', portNumber.toString(),
+                '--deploy-url', '/dist/', // Value should come from .angular-cli.json, but https://github.com/angular/angular-cli/issues/7347
+                '--extract-css'
+            ]);
+            devServerProc.stdout.pipe(process.stdout);
+
+            // Wait until the CLI dev server is listening before letting ASP.NET start the app
+            console.log('Waiting for @angular/cli service to start...');
+            var readySignal = /open your browser on (http\S+)/;
+            var lineReader = readline
+                .createInterface({ input: devServerProc.stdout })
+                .on('line', function (line) {
+                    var matches = readySignal.exec(line);
+                    if (matches) {
+                        var devServerUrl = url.parse(matches[1]);
+                        console.log('@angular/cli service has started on internal port ' + devServerUrl.port);
+                        callback(null, {
+                            Port: parseInt(devServerUrl.port),
+                            PublicPaths: [devServerUrl.path]
+                        });
+                        lineReader.close();
+                    }
+                });
         });
     }
 };
 
 function executeAngularCli(args) {
     var angularCliBin = require.resolve('@angular/cli/bin/ng');
-    process.argv = process.argv.slice(0, 1);
-    process.argv.push(angularCliBin);
-    Array.prototype.push.apply(process.argv, args);
-    require(angularCliBin);
+    return childProcess.fork(angularCliBin, args, {
+        stdio: [/* stdin */ 'ignore', /* stdout */ 'pipe', /* stderr */ 'inherit', 'ipc']
+    });
+}
+
+function getOSAssignedPortNumber() {
+    return new Promise(function (resolve, reject) {
+        var server = net.createServer();
+        server.listen(0, 'localhost', function () {
+            var portNumber = server.address().port;
+            server.close(function () { resolve(portNumber); });
+        });
+    });
 }
